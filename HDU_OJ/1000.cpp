a256559f4609d5694e38fd9a01b6caa439b28cff
xuweizhen <xxuweizhen@qq.com>
2019-03-31 15:55:53 +0800

    Create 1000.cpp

---
+++ HDU_OJ/1000.cpp
@@ -0,0 +1,19 @@
+
+//题目：http://acm.hdu.edu.cn/showproblem.php?pid=1000
+//时间：2017.2.15
+//思路：略
+
+
+#include <iostream>
+
+using namespace std ;
+
+int main()
+{
+	int a, b ;
+	
+	while(cin >> a >> b)
+		cout << a+b << endl;
+	
+	return 0;
+}
