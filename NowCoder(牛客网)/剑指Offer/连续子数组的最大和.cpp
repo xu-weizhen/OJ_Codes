3020b20abfcc73932f3df4b88124efe51afe382e
xuweizhen <xxuweizhen@qq.com>
2019-10-24 19:01:31 +0800

    Create 连续子数组的最大和.cpp

---
+++ NowCoder(牛客网)/剑指Offer/连续子数组的最大和.cpp
@@ -0,0 +1,26 @@
+/*
+**链接：https://www.nowcoder.com/questionTerminal/459bd355da1549fa8a49e350bf3df484
+**描述：HZ偶尔会拿些专业问题来忽悠那些非计算机专业的同学。今天测试组开完会后,他又发话了:在古老的一维模式识别中,常常需要计算连续子向量的最大和,当向量全为正数的时候,问题很好解决。但是,如果向量中包含负数,是否应该包含某个负数,并期望旁边的正数会弥补它呢？例如:{6,-3,-2,7,-15,1,2,2},连续子向量的最大和为8(从第0个开始,到第3个为止)。给一个数组，返回它的最大连续子序列的和，你会不会被他忽悠住？(子向量的长度至少是1)
+**时间：2019.10.23
+**思路：略
+*/
+
+class Solution {
+public:
+    int FindGreatestSumOfSubArray(vector<int> array) {
+        int length = array.size();
+        
+        int sum = 0;
+        int result = array[0];
+        for(int i=0; i<length; i++)
+        {
+            sum += array[i];
+            if(result < sum)
+                result = sum;
+            if(sum < 0)
+                sum = 0;
+        }
+        
+        return result;
+    }
+};
