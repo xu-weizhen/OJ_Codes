cc86117c6fa3e7825e17ed00e658f83bc2972032
xuweizhen <xxuweizhen@qq.com>
2019-10-29 17:05:41 +0800

    Create 序列化二叉树.cpp

---
+++ NowCoder(牛客网)/剑指Offer/序列化二叉树.cpp
@@ -0,0 +1,84 @@
+/*
+**链接：https://www.nowcoder.com/questionTerminal/cf7e25aa97c04cc1a68c8f040e71fb84
+**描述：请实现两个函数，分别用来序列化和反序列化二叉树
+
+二叉树的序列化是指：把一棵二叉树按照某种遍历方式的结果以某种格式保存为字符串，从而使得内存中建立起来的二叉树可以持久保存。序列化可以基于先序、中序、后序、层序的二叉树遍历方式来进行修改，序列化的结果是一个字符串，序列化时通过 某种符号表示空节点（#），以 ！ 表示一个结点值的结束（value!）。
+
+二叉树的反序列化是指：根据某种遍历顺序得到的序列化字符串结果str，重构二叉树。
+**时间：2019.10.29
+**思路：略
+*/
+
+/*
+struct TreeNode {
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
+    TreeNode(int x) :
+            val(x), left(NULL), right(NULL) {
+    }
+};
+*/
+class Solution {
+public:
+    char* Serialize(TreeNode *root)
+    {
+       if(root == NULL)
+           return NULL;
+        string str;
+        Serialize(root, str);
+        char *ret = new char[str.length() + 1];
+        int i;
+        for(i = 0; i < str.length(); i++)
+            ret[i] = str[i];
+        ret[i] = '\0';
+        return ret;
+    }
+    
+    void Serialize(TreeNode *root, string& str)
+    {
+        if(root == NULL)
+        {
+            str += '#';
+            return ;
+        }
+        string r = to_string(root->val);
+        str += r;
+        str += ',';
+        Serialize(root->left, str);
+        Serialize(root->right, str);
+    }
+     
+    TreeNode* Deserialize(char *str) 
+    {
+        if(str == NULL)
+            return NULL;
+        TreeNode *ret = Deserialize(&str);
+ 
+        return ret;
+    }
+    
+    TreeNode* Deserialize(char **str)
+    {//由于递归时，会不断的向后读取字符串
+        if(**str == '#')
+        {  //所以一定要用**str,
+            ++(*str);         //以保证得到递归后指针str指向未被读取的字符
+            return NULL;
+        }
+        int num = 0;
+        while(**str != '\0' && **str != ',')
+        {
+            num = num*10 + ((**str) - '0');
+            ++(*str);
+        }
+        
+        TreeNode *root = new TreeNode(num);
+        if(**str == '\0')
+            return root;
+        else
+            (*str)++;
+        root->left = Deserialize(str);
+        root->right = Deserialize(str);
+        return root;
+    }
+};
